d1ee48d5fe1b23ff0a321670a74ed5787931b3f5
wlcookies <17701457079@189.cn>
2021-01-23 15:00:37 +0800

    测试

--- app/src/main/java/com/wlcookies/funble/BleUUID.kt
+++ app/src/main/java/com/wlcookies/funble/BleUUID.kt
@@ -7,6 +7,7 @@ val WRITE_CHARACTER_UUID = UUID.fromString("6e400002-b5a3-f393-e0a9-e50e24dcca9e
 val NOTIFY_CHARACTER_UUID = UUID.fromString("6e400003-b5a3-f393-e0a9-e50e24dcca9e")
 val NOTIFY_DESCRIPTOR_UUID = UUID.fromString("00002902-0000-1000-8000-00805f9b34fb")
 
+
 /*
 服务UUID：6e400001-b5a3-f393-e0a9-e50e24dcca9e
     特征UUID：6e400002-b5a3-f393-e0a9-e50e24dcca9e   RX Characteristic 写
